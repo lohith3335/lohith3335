5b64d288fa59ef6c7dd0fb7b338ea61568d25f9d
lohith3335 <manyamlohithkumarreddy@gmail.com>
2025-06-11 21:10:47 +0530

    Add files via upload

---
+++ sortStringsAlphabetically.java
@@ -0,0 +1,16 @@
+package ABC;
+
+import java.util.*;
+public class sortStringsAlphabetically{
+public static void main(String[] args) {
+	ArrayList<String>pets=new ArrayList<>();
+	pets.add("cat");
+	pets.add("dog");
+	pets.add("cow");
+	System.out.println(pets);
+	pets.sort(null);
+	System.out.println(pets);
+	
+}
+	
+}

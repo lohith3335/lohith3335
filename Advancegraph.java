e0b3dce9fbc2d00155f6fc86167f6525fe47e8b0
lohith3335 <manyamlohithkumarreddy@gmail.com>
2025-06-11 20:42:54 +0530

    Add files via upload

---
+++ Advancegraph.java
@@ -0,0 +1,15 @@
+package ABC;
+import java.util.*;
+public class Advancegraph {
+	static class graph{
+		private Map<Integer,list<Integer>> adjlist=new HashMap<Integer,List<Integer>>();
+		void addEdge(int u,int v) {
+			adjlist.computei
+	}
+
+	
+		// TODO Auto-generated method stub
+
+	}
+
+}

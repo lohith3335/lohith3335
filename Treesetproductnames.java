777d2043fafa141d9cca95ccd58c8e9461d4724f
lohith3335 <manyamlohithkumarreddy@gmail.com>
2025-06-11 21:11:52 +0530

    Add files via upload

--- Treesetproductnames.java
+++ Treesetproductnames.java
@@ -1,14 +1,23 @@
 package ABC;
-
+import java.util.*;
 public class Treesetproductnames {
 
 	public static void main(String[] args) {
-		Treeset<String>productname=new Treeset<>();
-		Scanner sc=new scanner(system.in);
-		product name.add("keyboard");
-		product name.add("mouse");
-		product name.add("monitor");
-		product name.add("cpu");
+		TreeSet<String>productnames=new TreeSet<>();
+		Scanner sc=new Scanner(System.in);
+		productnames.add("keyboard");
+		productnames.add("mouse");
+		productnames.add("monitor");
+		productnames.add("cpu");
+		System.out.println("Available productnames:"+productnames);
+		System.out.println("Enter product to search:");
+		String input=sc.nextLine();
+		if(productnames.contains(input)) {
+			System.out.println("Found:"+input);
+			
+		}
+		sc.close();
+		
 		// TODO Auto-generated method stub
 
 	}

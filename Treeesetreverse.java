9d2496b842f5ddabca42328815fa0fc5a6e9da02
lohith3335 <manyamlohithkumarreddy@gmail.com>
2025-06-11 20:50:48 +0530

    Add files via upload

---
+++ Treeesetreverse.java
@@ -0,0 +1,20 @@
+package ABC;
+import java.util.*;
+public class Treeesetreverse {
+
+	public static void main(String[] args) {
+		TreeSet<String>employename=new TreeSet<>();
+		employename.add("sumit");
+		employename.add("karan");
+		employename.add("anjali");
+		employename.add("mohan");
+		employename.add("zoya");
+		System.out.println(employename.reversed());
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}

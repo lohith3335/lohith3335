bbace7b40e02dec0c558721d765b444b7b581cf3
lohith3335 <manyamlohithkumarreddy@gmail.com>
2025-06-11 20:56:18 +0530

    Add files via upload

---
+++ Treesetproductnames.java
@@ -0,0 +1,16 @@
+package ABC;
+
+public class Treesetproductnames {
+
+	public static void main(String[] args) {
+		Treeset<String>productname=new Treeset<>();
+		Scanner sc=new scanner(system.in);
+		product name.add("keyboard");
+		product name.add("mouse");
+		product name.add("monitor");
+		product name.add("cpu");
+		// TODO Auto-generated method stub
+
+	}
+
+}

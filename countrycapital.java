b119a0512442d1acc872d7684d8109532cd5859c
lohith3335 <manyamlohithkumarreddy@gmail.com>
2025-06-11 20:47:16 +0530

    Add files via upload

---
+++ countrycapital.java
@@ -0,0 +1,16 @@
+package ABC;
+import java.util.*;
+
+public class countrycapital {
+
+	public static void main(String[] args) {
+		TreeMap<String,String>countryname=new TreeMap<>();
+		countryname.put("India","newdelhi");
+		countryname.put("usa","washington");
+		countryname.put("germany","berlian");
+		countryname.put("japan","tokyo");
+		// TODO Auto-generated method stub
+
+	}
+
+}

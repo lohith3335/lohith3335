b3abba82eb2d7d83aa7451785dfe93c60c7a2938
lohith3335 <manyamlohithkumarreddy@gmail.com>
2025-06-17 00:04:32 +0530

    Add files via upload

---
+++ inputexample.java
@@ -0,0 +1,15 @@
+package ABC;
+import java.util.*;
+public class inputexample {
+	 public static void main (String []args) {
+	    	Scanner sc =new Scanner(System.in);
+	    	System.out.println("enter the age:");
+	    	int age=sc.nextInt();
+	    	sc.nextLine();
+	    	System.out.println("enter the name:");
+	    	String name=sc.nextLine();
+	    	System.out.println(name+age);
+	    	}
+	}
+
+

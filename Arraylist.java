9ae4860902d2b445efb7ab7ab96a8f7c2f33efb7
lohith3335 <manyamlohithkumarreddy@gmail.com>
2025-06-16 23:58:13 +0530

    Add files via upload

---
+++ Arraylist.java
@@ -0,0 +1,23 @@
+package ABC;
+
+import java.util.ArrayList;
+
+public class Arraylist {
+	
+	
+
+		public static void main(String[] args) {
+			ArrayList<String> students=new ArrayList<String>();
+			students.add("kamal");
+			students.add("latha");
+			students.add("kamal");
+			students.add("jai");
+			students.add("malini");
+			System.out.println(students);
+
+		}
+
+	}
+
+
+

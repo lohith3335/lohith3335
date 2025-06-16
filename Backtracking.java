8904b4888a5b2faa4603cd5a88a81ca93dd59136
lohith3335 <manyamlohithkumarreddy@gmail.com>
2025-06-16 23:57:38 +0530

    Add files via upload

---
+++ Backtracking.java
@@ -0,0 +1,21 @@
+package ABC;
+
+public class Backtracking {
+	
+	    public static void makeWords(String current, String letters) {
+	        if (current.length() == 3) {
+	            System.out.println(current);
+	            return;
+	        }
+
+	        for (int i = 0; i < letters.length(); i++) {
+	            makeWords(current + letters.charAt(i), letters);
+	        }
+	    }
+
+	    public static void main(String[] args) {
+	        makeWords(" ","ABC");;
+	}
+	}
+
+
